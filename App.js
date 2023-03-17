ec9a48c915c567bb143489ee23776c21f558ca2b
Adom7 <adlaneould@gmail.com>
2023-03-17 15:29:03 +0100

    Learning Caroussel

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button , TextInput} from 'react-native';
-
-
-
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [enteredGoalText , setEnteredGoalText] = useState('');
+  const [enteredGoalText, setEnteredGoalText] = useState('');
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText)
   };
@@ -14,18 +11,28 @@ export default function App() {
   };
 
   return (
-
-
-
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course Goal !" onChangeText={goalInputHandler}/>
+        <TextInput style={styles.textInput} placeholder="Your course Goal !" onChangeText={goalInputHandler} />
         <Button title='Add Goal' onPress={addGoalHandler} />
+      </View>
+      <View style={styles.caroussel} >
+        <View style={styles.square} ></View>
+        <View style={styles.square} ></View>
+        <View style={styles.square} ></View>
+        <View style={styles.square} ></View>
+        <View style={styles.square} ></View>
+        <View style={styles.square} ></View>
+        <View style={styles.square} ></View>
+        <View style={styles.square} ></View>
+        <View style={styles.square} ></View>
+        <View style={styles.square} ></View>
+        <View style={styles.square} ></View>
+
       </View>
       <View style={styles.goalsContainer}>
         <Text  >List of Goals Maybe </Text>
         <Text  > {enteredGoalText}  </Text>
-        <Text  >List of Goals Maybe </Text>
         <Text  >List of Goals Maybe </Text>
       </View>
     </View >
@@ -33,30 +40,42 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  appContainer : {
-    flex : 1 ,
+  appContainer: {
+    flex: 1,
     paddingTop: 50,
-    paddingHorizontal : 16,
+    paddingHorizontal: 16,
   },
-  inputContainer : {
-    flex : 1,
-    flexDirection : 'row',
-    justifyContent : 'space-between',
-    alignItems : 'center',
-    marginBottom : 24,
-    borderBottomWidth : 1 , 
-    borderColor : 'orange',
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: 'orange',
 
   },
-  textInput : {
-    borderWidth : 1,
-    borderColor : '#cccccc',
-    width : '70%',
-    marginRight : 8,
-    padding : 8
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
   },
-  goalsContainer : {
-    flex : 5, 
-    alignItems : 'center'
+  goalsContainer: {
+    flex: 5,
+    alignItems: 'center'
+  },
+  caroussel: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginBottom: 20,
+  },
+  square: {
+    borderRadius: 10,
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
+
   },
 });
