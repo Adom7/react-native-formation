49556bdfecd3acc85c9903eabecbc4e6bd2a2e1e
Adom7 <Adlaneould@gmail.com>
2023-03-17 01:34:54 +0100

    Half Course

--- App.js
+++ App.js
@@ -1,47 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button , TextInput} from 'react-native';
 
 
 
 
 export default function App() {
-
-  const [text, setText] = useState(' World');
-  const [value, setValue] = useState(0);
-
+  const [enteredGoalText , setEnteredGoalText] = useState('');
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText)
+  };
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  };
 
   return (
-    <View style={styles.container} >
-      <Text> {text} </Text>
-      <Button
-        onPress={() => { setText('Je suis en train de faire un test peut etre que ca fontionne maintenant') }}
-        title='Change le text :)'
-      >
 
-      </Button>
-      <Text> { value } </Text>
-      <Button
-        onPress={() => { setValue( value + 1) }}
-        title='+1'
-       />
+
+
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course Goal !" onChangeText={goalInputHandler}/>
+        <Button title='Add Goal' onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text  >List of Goals Maybe </Text>
+        <Text  > {enteredGoalText}  </Text>
+        <Text  >List of Goals Maybe </Text>
+        <Text  >List of Goals Maybe </Text>
+      </View>
     </View >
-
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer : {
+    flex : 1 ,
+    paddingTop: 50,
+    paddingHorizontal : 16,
   },
-  button: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 4,
-  }
+  inputContainer : {
+    flex : 1,
+    flexDirection : 'row',
+    justifyContent : 'space-between',
+    alignItems : 'center',
+    marginBottom : 24,
+    borderBottomWidth : 1 , 
+    borderColor : 'orange',
+
+  },
+  textInput : {
+    borderWidth : 1,
+    borderColor : '#cccccc',
+    width : '70%',
+    marginRight : 8,
+    padding : 8
+  },
+  goalsContainer : {
+    flex : 5, 
+    alignItems : 'center'
+  },
 });
